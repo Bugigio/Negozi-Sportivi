7c373b529711092be624f95dac7951ff4fd17d80
Bugigio <gabrieletommasi04@gmail.com>
2023-05-10 12:46:11 +0200

    Create home.php
    tested some css

---
+++ web pages/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Home</title>
+		<style>
+			header {
+				display: flex;
+				flex-direction: row;
+			}
+		</style>
+	</head>
+	<body>
+		<header>
+			<div>
+				<a href="shop1.php"><button>shop 1</button></a>
+			</div>
+			<div><a href="shop2.php"><button>shop 2</button></a></div>
+			<div><a href="shop3.php"><button>shop 3</button></a></div>
+			<div><a href="shop4.php"><button>shop 4</button></a></div>
+			<div>
+				<a href="login.php"><button>Login</button></a>
+			</div>
+			<div>
+				<a href="signIn.php"><button>Registrati</button></a>
+			</div>
+		</header>
+		<h1>Benvenuto</h1>
+	</body>
+</html>
