113916982e5f47b5d152ec8e8e82c6a215a8d73e
cighii <122048559+cighii@users.noreply.github.com>
2023-05-17 16:50:41 +0200

    Newsletter
    Created a newsletter form.

---
+++ newsletter.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEWSLETTER</title>
+</head>
+<body>
+    <form>
+        <h2>Login</h2>
+        <div class="inputbox">
+            <ion-icon name="person-circle-outline"></ion-icon>
+            <input type="mail" name="mail" required>
+            <label for="uname">email</label>
+        </div>
+            <input type="checkbox" name="" value="" required/> accetti di ricevere notizie relative ai nuovi prodotti.<br/>
+            <input type="checkbox" name="" value="" required/> accetti di ricevere notizie da siti di terze parti.<br/>
+            <button>Registrati</button>
+        <div class="register">
+            <p>Non hai un account? <a href="registrazione.php">Registrati ora</a></p>
+        </div>
+    </form>
+</body> 
+</html>
