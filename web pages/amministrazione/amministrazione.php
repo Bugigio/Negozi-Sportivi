c44bc477a3f9522437768794b6a641798f08516d
alextanase2004 <alextanase2378@gmail.com>
2023-05-18 16:49:30 +0200

    Created amministration page
    Create 'amministrazione' folder and its pages.

---
+++ web pages/amministrazione/amministrazione.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Amministrazione</title>
+    </head>
+    <body>
+        <h1>Amministrazione</h1>
+        <form action=amministrazione.php method=post>
+            <input type="button" value="logout"></input>
+        </form>
+        <div class=logout><a href=home.php></a></div>
+    </body>
+</hmtl>
