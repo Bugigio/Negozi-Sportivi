c70a36389f987489622b3b5da90a831a019ef2c5
Bugigio <gabrieletommasi04@gmail.com>
2023-06-04 18:10:31 +0200

    added functions
    started the function to add a product, added the error function and fixed some problems with displaying the products after they have been modified

--- web pages/amministrazione/amministrazioneM.php
+++ web pages/amministrazione/amministrazioneM.php
@@ -24,12 +24,28 @@
     }
 	session_start();
     if(isset($_POST['magazzino'])) {
-        $_SESSION['magazzino'] = true;
+        $_SESSION['magazzino'] = $_POST['magazzino'];
     }
 	if(!isset($_SESSION['magazzino'])) {
 		header("location: amministrazione.php");
 		die();
 	}
+	if(isset($_REQUEST['err'])) {
+		echo '<script> window.alert("';
+		switch ($_REQUEST['err']) {
+			case '1':
+				echo 'Offerta non esistente';
+				break;
+			case '2':
+				echo "Un articolo non può avere un'offerta e un rincaro";
+				break;
+			default:
+				echo 'Modifica eseguita con successo';
+				break;
+		}
+		echo '");</script>';
+	}
+
 ?>
 <html>
 	<body>
@@ -46,8 +62,8 @@
 			</div>
 		</header>
         <h1>AMMINISTRAZIONE MAGAZZINO</h1>
-		<form action="#" method="post">
-			<input type="hidden" name="magazzino" value="<?php if(isset($_POST['magazzino'])) echo $_POST['magazzino']; ?>">
+		<form action="amministrazioneM.php" method="post">
+			<input type="hidden" name="magazzino" value="<?php if(isset($_SESSION['magazzino'])) echo $_SESSION['magazzino']; ?>">
 			<input type="submit" name="submit" value="Articoli">
 			<input type="submit" name="submit" value="Utenti">
 			<input type="submit" name="submit" value="Ordini">
@@ -57,32 +73,41 @@
 			<input type="submit" name="submit" value="Newsletter">
 		</form>
 		<?php 
-			if(isset($_POST['submit'])) {
+			if(isset($_REQUEST['submit'])) {
 				$db = new mysqli("localhost", "root", "", "accessport");
-				switch ($_POST['submit']) {
+				switch ($_REQUEST['submit']) {
 					case 'Articoli':
 						?>
 						<table border="1">
 							<tr><td>Articoli</td></tr>
 							<tr><td>ID_articolo</td><td>nome_articolo</td><td>tipo_articolo</td><td>quantita</td><td>prezzo acquisto</td><td>prezzo vendita</td><td>rincaro</td><td>codice offerta</td></tr>
 							<?php 
-							$query_articoli = "SELECT * FROM articolo WHERE nome_magazzino LIKE '" . $_POST['magazzino'] . "';";
+							$query_articoli = "SELECT * FROM articolo WHERE nome_magazzino LIKE '" . $_SESSION['magazzino'] . "';";
 							$articoli = $db->query($query_articoli);
-
-
 							foreach($articoli as $a) {
 								?>
 								<form action="#" method="post">
 								<?php
-								echo "<tr><td><input type='number' name='ID_articolo' value='" . $a["ID_articolo"] . "' readonly /></td><td><input type='text' name='nome_articolo' value='" . $a["nome_articolo"] . "'/></td><td><input type='text' name='tipo_articolo' value='" . $a["tipo_articolo"] . "'/></td><td><input type=number name='quantita' value='" . $a["quantita"] . "'/></td><td><input type=text name='prezzo_acquisto' value='" . $a["prezzo_acquisto"] . "' readonly/></td><td><input type=text name='prezzo_vendita' value='" . $a["prezzo_vendita"] . "'/></td><td><input type=number min=0 max=100 name=rincaro value='" . $a["rincaro"] . "'/></td><td><input type=number value='" . $a["cod_offerta"] . "' readonly/></td><td><input type=submit name=rimuovi_articolo value=Rimuovi /></td><td><input type=submit name=modifica_articolo value=Modifica /></td></tr>";
+								echo "<tr><td><input type='number' name='ID_articolo' value='" . $a["ID_articolo"] . "' readonly /></td><td><input type='text' name='nome_articolo' value='" . $a["nome_articolo"] . "'/></td><td><input type='text' name='tipo_articolo' value='" . $a["tipo_articolo"] . "'/></td><td><input type=number name='quantita' value='" . $a["quantita"] . "'/></td><td><input type=text name='prezzo_acquisto' value='" . $a["prezzo_acquisto"] . "' readonly/></td><td><input type=text name='prezzo_vendita' value='" . $a["prezzo_vendita"] . "'/></td><td><input type=number min=0 max=100 name=rincaro value='" . $a["rincaro"] . "'/></td><td><input type=number min=1 name=cod_offerta value='" . $a["cod_offerta"] . "' /></td><td><input type=submit name=rimuovi_articolo value=Rimuovi /></td><td><input type=submit name=modifica_articolo value=Modifica /></td></tr>";
 								?>
 								</form>
 								<?php
 							}
 							?>
-							</form>
 						</table>
-						
+						<form action="amministrazione.php" method="post">
+							<table>
+								<tr><td>Aggiungi un nuovo articolo</td></tr>
+								<tr><td><input type="text" name="nome_articolo" placeholder="Nome articolo" required/></td></tr>
+								<tr><td><input type="text" name="tipo_articolo" placeholder="Tipo articolo" required/></td></tr>
+								<tr><td><input type="number" min=1 name="quantita" placeholder="Quantità" required/></td></tr>
+								<tr><td><input type="text" name="prezzo_acquisto" placeholder="Prezzo acquisto" required/></td></tr>
+								<tr><td><input type="text" name="prezzo_vendita" placeholder="Prezzo vendita" required/></td></tr>
+								<tr><td><input type="number" name="rincaro" min="0" max="100" placeholder="Rincaro (%)"/></td></tr>
+								<tr><td><input type="number" name="cod_offerta" min="1" placeholder="Codice offerta" /></td></tr>
+								<tr><td><input type="submit" name="aggiungi" value="Aggiungi"></td></tr>
+							</table>
+						</form>
 
 						<?php
 						break;
@@ -98,13 +123,12 @@
 								?>
 								<form action="#" method="post">
 								<?php
-								echo '<tr><td><input type="email" name="email" value="' . $u["email"] . '" readonly></td><td><input type="text" name="nome" value="' . $u["nome"] . '" readonly></td><td><input type="text" name="cognome" value="' . $u["cognome"] . '" readonly></td><td><input type="text" name="citta" value="' . $u["citta"] . '" readonly></td><td><input type="text" name="via" value="' . $u["via"] . '" readonly></td><td><input type="number" name="numero_civico" value="' . $u["numero_civico"] . '" readonly></td><td><input type="text" name="provincia" value="' . $u["provincia"] .'" readonly></td><td><input type=submit name=rimuovi_utente value=Rimuovi /></td></tr>';
+								echo '<tr><td><input type="email" name="email" value="' . $u["email"] . '" readonly /></td><td><input type="text" name="nome" value="' . $u["nome"] . '" readonly /></td><td><input type="text" name="cognome" value="' . $u["cognome"] . '" readonly /></td><td><input type="text" name="citta" value="' . $u["citta"] . '" readonly /></td><td><input type="text" name="via" value="' . $u["via"] . '" readonly /></td><td><input type="number" name="numero_civico" value="' . $u["numero_civico"] . '" readonly /></td><td><input type="text" name="provincia" value="' . $u["provincia"] .'" readonly /></td><td><input type=submit name=rimuovi_utente value=Rimuovi /></td></tr>';
 								?>
 								</form>
 								<?php
 							}
 							?>
-							
 						</table>
 						<?php
 						break;
@@ -124,13 +148,38 @@
 						# code...
 						break;
 				}
-			} else if(isset($_POST['rimuovi_articolo'])) {
+				$db->close();
+			} else if(isset($_POST['modifica_articolo'])) { // cambiamenti articolo
+				$db = new mysqli("localhost", "root", "", "accessport");
+				$query_offerte = "SELECT * FROM offerte WHERE ID_offerta = " . $_POST['cod_offerta'] . ";";
+				$offerte = $db->query($query_offerte);
+				if($offerte->num_rows === 0) {
+					header("location: amministrazioneM.php?err=1");
+					die();
+				}
+				if($_POST['cod_offerta'] != 0 && $_POST['rincaro'] != 0) {
+					header("location: amministrazioneM.php?err=2");
+					die();
+				}
+				$query = "UPDATE articolo SET nome_articolo = '" .  $_POST['nome_articolo'] . "', tipo_articolo = '" . $_POST['tipo_articolo'] . "', quantita = '" . $_POST['quantita'] . "', prezzo_vendita = '" . $_POST['prezzo_vendita'] . "', rincaro = '" . $_POST['rincaro'] . "', cod_offerta = '" . $_POST['cod_offerta'] . "' WHERE ID_articolo = " . $_POST['ID_articolo'] . ";";
+				$db->query($query);
+				$db->close();
+				header("location: amministrazioneM.php?err");
+				die();
+			}
+			if(isset($_POST['aggiungi'])) {
+				$db = new mysqli("localhost", "root", "", "accessport");
+				$query_inserimento = "INSERT INTO articolo"; // da continuare
+			}
+			if(isset($_POST['rimuovi_articolo'])) {
 				$id_articolo = $_POST['ID_articolo'];
 				$db = new mysqli("localhost", "root", "", "accessport");
 				$query_rimozione_acquisti = "DELETE FROM acquistare WHERE id_articolo = $id_articolo;";
 				$db->query($query_rimozione_acquisti);
 				$query = "DELETE FROM articolo WHERE ID_articolo = $id_articolo;";
 				$db->query($query);
+				header("location: amministrazioneM.php?err");
+				die();
 			}
 		?>
     </body>
